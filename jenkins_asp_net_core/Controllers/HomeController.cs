f172ef4c1a0cacf4061297beb022e882abc7285d
NgocLeQuang <quangngoc1695910@gmail.com>
2023-12-10 14:54:15 +0700

    1.0.5

--- jenkins_asp_net_core/Controllers/HomeController.cs
+++ jenkins_asp_net_core/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace jenkins_asp_net_core.Controllers
 
         public IActionResult Index()
         {
-			ViewData["data"]="sonarqube in jenkins and docker v1.0.4";
+			ViewData["data"]="sonarqube in jenkins and docker v1.0.5";
             return View();
         }
 
